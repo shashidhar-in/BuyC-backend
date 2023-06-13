c19c505aca0f9ae0b9681dcc72e210a945bbb423
Shashidhar Mittapalli <shashidhar.mittapalli@gmail.com>
2023-06-13 13:19:20 +0530

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const carRoutes = require('./routes/carRoutes');
 require('dotenv').config();
 const app = express();
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
@@ -38,6 +38,9 @@ app.listen(process.env.PORT, () => {
 });
 
 });
+// Handle preflight requests for the /api/users/login route
+app.options('/api/users/signup', cors({ origin: 'https://buyc-ssd.netlify.app', credentials: true }));
+app.options('/api/users/login', cors({ origin: 'https://buyc-ssd.netlify.app', credentials: true }));
 
 
 // Create the userRoutes with the pool and use it
