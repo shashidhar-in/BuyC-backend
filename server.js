9966a7765be81f23af10737f2ad8ab6a63eb614b
Shashidhar Mittapalli <shashidhar.mittapalli@gmail.com>
2023-06-13 13:13:30 +0530

    git

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const carRoutes = require('./routes/carRoutes');
 require('dotenv').config();
 const app = express();
 app.use(cookieParser());
-app.use(cors({
-  origin:["http://localhost:3000","https://buyc-ssd.netlify.app/"],
-  credentials:true,
-}))
+app.use(cors())
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
